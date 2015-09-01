231a4d9494561b1c2be981d5da225aef5c6dcc1a
Marcin Dryka <m.dryka@clearcode.cc>
2015-09-01 16:02:08 +0200

    PHPSpec

---
+++ src/AppBundle/spec/AppBundle/Controller/UserControllerSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace spec\AppBundle\Controller;
+
+use AppBundle\Controller\UserController;
+use FOS\RestBundle\Util\Codes;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @mixin UserController
+ *
+ * @method beConstructedWith(UserInterface $user)
+ */
+class UserControllerSpec extends ObjectBehavior
+{
+    function let(UserInterface $user)
+    {
+        $this->beConstructedWith($user);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('AppBundle\Controller\UserController');
+    }
+
+    function it_generate_profile(UserInterface $user)
+    {
+        $this->getProfileAction()->shouldBeRestViewWith([
+            'data' => $user,
+            'statusCode' => Codes::HTTP_OK,
+            'serializationGroups' => ['user_profile'],
+            'headers' => [
+                'cache-control' => ['no-cache'],
+                'date' => ["@string@.isDateTime()"],
+            ]
+        ]);
+    }
+}
